3e922f3a10bffe5289caf42a1f71bbe70a0b4824
trvs222 <101153438+trvs222@users.noreply.github.com>
2022-06-30 19:08:01 -0400

    Initial commit

---
+++ Scripts/cointoss.js
@@ -0,0 +1,42 @@
+// DOM Elements
+
+// Initialize counts for our coin flip tally
+
+
+// Click event listener for our flip button 
+
+  // Assign random number between 0 and 1 to a variable
+  
+  // In line stylee setting animation to "none"
+  
+  // console logging the result of the flip
+ 
+  // if the random number is greater than .5 we're in this if statement
+  
+    //starting the heads flip animation after a delay
+  
+    //incrementing the heads count by 1
+    
+    // if the random number is less than .5 we're in this if statement
+ 
+    //starting the tails flip animation after a delay
+
+    //incrementing the tails count by 1
+   
+  // Disabling the flip button while the coin is flipping
+ 
+  // Update the flip tally once the coin has stopped flipping
+ 
+  // Re-enable the flip button after 3 seconds
+  
+
+// Adding a click event listener to our reset button
+
+  // Reset the flip tally to zero
+  
+  // Update the score text content
+
+  // Re-enable the flip button
+ 
+  // Reset the coin flip animation
+
